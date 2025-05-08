1c36bfed7d3cfb43e693a8ea0ecfa29a0e6eda20
OLDHel1Otrope <uppathak42@gmail.com>
2025-05-08 18:49:23 +0530

    disable approvals on normal user

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,15 +2,17 @@ import DashboardHeader from "../components/DashboardHeader";
 import PendingApprovals from "../components/PendingApprovals";
 import Contacts from "../components/Contacts";
 import ActionsModal from "../components/ActionsModal";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { MyTeam } from "../components/MyTeam";
 import { Notices } from "../components/Notices";
 import { LeavesCalenderHolder } from "../components/LeavesCalenderHolder";
 import WelcomeModal from "../components/WelcomeModal";
+import { UserContext } from "../contexts/UserContextProvider";
 
 function Home() {
   const [isModalOpen, setModalOpen] = useState(false);
   const [modalType, setModalType] = useState(null);
+  const { isSuperUser } = useContext(UserContext);
 
   return (
     <div className="min-h-screen flex flex-col md:flex-row pt-4 justify-center bg-stone-950 px-10 pl-20 gap-4">
@@ -22,7 +24,7 @@ function Home() {
           setModalType={setModalType}
         />
 
-        <PendingApprovals />
+        {isSuperUser && <PendingApprovals />}
         <Notices setModalOpen={setModalOpen} setModalType={setModalType} />
 
         <Contacts />
